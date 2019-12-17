458151d317d86bc5836f23721970a18013f96dcc
carlobutelli <dev.butelli@gmail.com>
2019-12-17 04:40:05 +0100

    Updated query (useless)

--- src/main/java/com/tech/repository/AirlineRepository.java
+++ src/main/java/com/tech/repository/AirlineRepository.java
@@ -1,11 +1,9 @@
 package com.tech.repository;
 
-import com.sun.xml.bind.v2.model.core.ID;
 import com.tech.model.Airline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,8 +12,8 @@ public interface AirlineRepository extends JpaRepository<Airline, Integer> {
     void deleteAirlineByIcaoCode(int id);
 
     @Modifying(clearAutomatically = true)
-    @Query(value = "UPDATE airline SET pDelayed = ?3, pCancelled = ?2 WHERE id = ?1", nativeQuery = true)
-    int updateProbabilities(int airlineId, int pCancelled, int pDelayed);
+    @Query(value = "UPDATE airline SET delayedProbability = ?3, cancelledProbability = ?2 WHERE id = ?1", nativeQuery = true)
+    int updateProbabilities(int airlineId, int cancelledProbability, int delayedProbability);
 
 
     Airline findOneById(int id);
