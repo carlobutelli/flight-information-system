220ddfd4202e636dc1d9d5aeaff44798257ce075
carlobutelli <dev.butelli@gmail.com>
2019-12-17 04:39:17 +0100

    Added query to retrieve airport by iata code

--- src/main/java/com/tech/repository/AirportRepository.java
+++ src/main/java/com/tech/repository/AirportRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirportRepository extends JpaRepository<Airport, String> {
+    Airport findOneByIataCode(String id);
 }
