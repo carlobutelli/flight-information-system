bdaa145096879db0c6529c8dc2fc90a55715b0b5
carlobutelli <dev.butelli@gmail.com>
2019-12-17 04:38:44 +0100

    Added find flight by source and destination

--- src/main/java/com/tech/repository/FlightRepository.java
+++ src/main/java/com/tech/repository/FlightRepository.java
@@ -4,6 +4,11 @@ import com.tech.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
+    List<Flight> findAllBySourceAndDestination(String source, String destination);
+    List<Flight> findAllBySource(String source);
+    List<Flight> findAllByDestination(String destination);
 }
